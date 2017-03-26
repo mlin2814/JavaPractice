302e35d4c9001ba22af9361eaca73c7909a4f34f
Matthew Lin <Matthew Lin>
2017-03-25 23:42:51 -0400

    Began work on learning methods

---
+++ Methods/src/com/Matthew/Main.java
@@ -0,0 +1,24 @@
+package com.Matthew;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 5000;
+            System.out.println("Your final score is: " + finalScore);
+            return finalScore;
+        }
+        else {
+            return -1;
+        }
+    }
+}
