e56cf9f04a4764e2fa25c331dd03b9d18ba94a20
Matthew Lin <Matthew Lin>
2017-04-03 13:05:08 -0400

    Started work on inheritance in Java

---
+++ Inheritance/src/com/Matthew/Dog.java
@@ -0,0 +1,33 @@
+package com.Matthew;
+
+/**
+ * Created by Matthew on 4/3/2017.
+ */
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tails;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tails, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tails = tails;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew(){
+        System.out.println("Dog chew called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog called eat");
+        chew();
+        super.eat();
+    }
+}
