13a2d07ff2f282d595f729ad5945d5827948f924
Matthew Lin <mlin2814@gmail.com>
2018-06-04 17:15:23 -0400

    Initial work on hamburger construction challenge

--- OOP Challenge 1/src/com/Matthew/Main.java
+++ OOP Challenge 1/src/com/Matthew/Main.java
@@ -4,27 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        char burgerType = 'R';
-        burgerType = 'H';
+//        char burgerType = 'R';
+//        burgerType = 'H';
+//
+//        switch (burgerType){
+//            case 'R':
+//                Regular regular = new Regular("Wheat", "Beef", "Regular",  5);
+//                System.out.println(regular.name);
+//                break;
+//
+//            case 'H':
+//                Healthy healthy = new Healthy("Brown Rye", "Chicken", "Healthy", 7, "Onions", 1, "Pickles", 1);
+//                System.out.println(healthy.name);
+//                break;
+//
+//            case 'D':
+//                System.out.println("Placeholder");
+//                break;
+//
+//            default:
+//                System.out.println("Please select which burger you would like");
+//                break;
+//        }
 
-        switch (burgerType){
-            case 'R':
-                Regular regular = new Regular("Wheat", "Beef", "Regular",  5);
-                System.out.println(regular.name);
-                break;
-
-            case 'H':
-                Healthy healthy = new Healthy("Brown Rye", "Chicken", "Healthy", 7, "Onions", 1, "Pickles", 1);
-                System.out.println(healthy.name);
-                break;
-
-            case 'D':
-                System.out.println("Placeholder");
-                break;
-
-            default:
-                System.out.println("Please select which burger you would like");
-                break;
-        }
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+        System.out.println(hamburger.itemizeHamburger());
     }
 }
